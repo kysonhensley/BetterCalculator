26dce789ec82dab323700463e272b62010dc1951
kysonhensley <hensleykyson@gmail.com>
2025-09-13 16:01:32 -0600

    Add project files.

---
+++ BetterCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BetterCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.");
+
+            while (userInput != "Q")
+            {
+                Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.");
+                Console.WriteLine("Press Q to quit");
+               
+            }
+
+
+
+
+            Console.Read();
+        }
+    }
+}
