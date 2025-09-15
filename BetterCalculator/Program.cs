d3ec6c373062d122d30acc4065230c447fab7056
kysonhensley <hensleykyson@gmail.com>
2025-09-14 22:44:33 -0600

    Finished

--- BetterCalculator/Program.cs
+++ BetterCalculator/Program.cs
@@ -1,26 +1,127 @@
-﻿namespace BetterCalculator
+﻿//Kyson Hensley
+//RCET2265
+//Fall 2025
+//Better Calculator Program
+//
+
+namespace BetterCalculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int firstNumber;
-            int secondNumber;
-            string userInput = Console.ReadLine();
 
-            Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.");
 
-            while (userInput != "Q")
+            int firstNumber = 0;
+            int secondNumber = 0;
+            bool userHasQuit = false;
+
+
+
+            while (!userHasQuit)
             {
-                Console.WriteLine("Please enter two numbers. Enter Q at any time to quit.");
-                Console.WriteLine("Press Q to quit");
-               
+                while (true)
+                {
+                    Console.WriteLine("Please enter two numbers. Enter \"Q\" at any time to quit.");
+                    Console.Write("Choose a Number: ");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == "Q")
+                    {
+                        userHasQuit = true;
+                        break;
+                    }
+                    
+
+                    if (int.TryParse(userInput, out firstNumber))
+                    {
+                        Console.WriteLine($"You entered \"{userInput}\"");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You entered \"{userInput}\", please enter a whole number.");
+                    }
+
+
+
+                }
+
+                if (userHasQuit) break;
+
+                while (true)
+                {
+                    Console.Write("Choose a Number: ");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == "Q")
+                    {
+                        userHasQuit = true;
+                        break;
+                    }
+                    
+
+                    if (int.TryParse(userInput, out secondNumber))
+                    {
+                        Console.WriteLine($"You entered \"{userInput}\"");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You entered \"{userInput}\", please enter a whole number.");
+                    }
+
+
+
+                }
+
+                    while (true)
+                    {
+                    Console.WriteLine("Choose one of the following options:");
+                    Console.WriteLine("1. Add");
+                    Console.WriteLine("2. Subtract");
+                    Console.WriteLine("3. Multiply");
+                    Console.WriteLine("4. Divide");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == "Q")
+                    {
+                        userHasQuit = true;
+                        break;
+                    }
+                   
+
+                        Console.WriteLine($"You entered \"{userInput}\"");
+                    bool validOperation = true;
+
+                    switch (userInput)
+                    {
+                        case "1":
+                            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                            break;
+                        case "2":
+                            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                            break;
+                        case "3":
+                            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                            break;
+                        case "4":
+                            Console.WriteLine($"{firstNumber} / {secondNumber} = {(double)firstNumber / secondNumber}");
+                            break;
+                        default:
+                            validOperation = false;
+                            break;
+
+                    }
+
+                    if (validOperation)
+                        break;
+                }
             }
 
-
-
-
-            Console.Read();
+            Console.WriteLine("Have a nice day!");
+            Console.WriteLine("Press enter to close this window");
+            Console.ReadLine();
         }
     }
 }
